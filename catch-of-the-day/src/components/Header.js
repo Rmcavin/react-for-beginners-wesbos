77e299a526e855d95daf018010764dcb273e24e8
Rachel <rmcavin@gmail.com>
2017-06-04 16:22:34 -0500

    Finished video 9

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
+//because the only method this component will ever use is render, we do not
+//need the full "class Header extends React.Component", instead we are using a
+//stateless function component, which is a more simple style of component.
+//See it on the line right below :)
+
+const Header = (props) => {
     return (
-      <p>Header</p>
+      <header className="top">
+        <h1>
+        Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+        Day
+        </h1>
+        <h3 className="tagline"><span>{props.tagline}</span></h3>
+      </header>
     )
-  }
 }
 
 export default Header;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';  //we don't need the whole package, only the render method!
+import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css';
 import App from './components/App'
 import StorePicker from './components/StorePicker'
+import NotFound from './components/NotFound'
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} /> {/*When the pattern is exactly a /, aka the home page, the component should be the store picker! */}
+        <Match pattern="/store/:storeid" component={App} /> {/*When a store is searched, the app component will render*/}
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
